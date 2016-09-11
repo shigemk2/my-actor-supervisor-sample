7c3bb9a7c59ec660cf05494af73c8625584d02df
shigemk2 <i.am.shige@gmail.com>
2016-09-11 23:28:29 +0900

    Add Hello

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,7 +1,24 @@
 package com.example
 
+import akka.actor.{ActorSystem, Props}
+
 object Hello {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    val system = ActorSystem("mySystem")
+
+    val props = Props[MyActor]
+    val actor = system.actorOf(props, name = "myActor")
+    Thread.sleep(1000)
+
+    actor ! "let's throw ArithmeticException"
+    Thread.sleep(1000)
+    actor ! "let's throw NullPointerException"
+    Thread.sleep(1000)
+    actor ! "let's throw IllegalArgumentException"
+    Thread.sleep(1000)
+    actor ! "let's throw Exception"
+    Thread.sleep(1000)
+
+    system.terminate
   }
 }
