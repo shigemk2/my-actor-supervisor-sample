9b611e3fe5c1ba9bcc959da1a5376f574a4088b4
shigemk2 <i.am.shige@gmail.com>
2016-09-11 22:36:09 +0900

    Add MyActor2

--- src/main/scala/com/example/MyActor2.scala
+++ src/main/scala/com/example/MyActor2.scala
@@ -1,5 +1,42 @@
 package com.example
 
-class MyActor2 {
+import akka.actor.Actor
+import akka.event.Logging
+
+class MyActor2 extends Actor {
+  val log = Logging(context.system, this)
+
+  override def preStart(): Unit = {
+    log.info("preStart")
+  }
+
+  override def preRestart(reason: Throwable, message: Option[Any]): Unit = {
+    log.info("preRestart")
+  }
+
+  override def postRestart(reason: Throwable): Unit = {
+    log.info("postRestart")
+  }
+
+  override def postStop(): Unit = {
+    log.info("postStop")
+  }
+
+  def receive = {
+    case "let's throw ArithmeticException" => {
+      throw new ArithmeticException
+    }
+    case "let's throw NullPointerException" => {
+      throw new NullPointerException
+    }
+    case "let's throw IllegalArgumentExceptoin" => {
+      throw new IllegalArgumentException
+    }
+    case "let's throw Exception" => {
+      throw new Exception
+    }
+    case _ => {
+    }
+  }
 
 }
