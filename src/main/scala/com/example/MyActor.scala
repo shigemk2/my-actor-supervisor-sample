55e2e073033a5561ba63bb63cc42617f55772fbc
shigemk2 <i.am.shige@gmail.com>
2016-09-11 22:46:19 +0900

    Add MyActor

--- src/main/scala/com/example/MyActor.scala
+++ src/main/scala/com/example/MyActor.scala
@@ -1,5 +1,30 @@
 package com.example
 
-class MyActor {
+import akka.actor.SupervisorStrategy.{Escalate, Restart, Resume, Stop}
+import akka.actor.{Actor, OneForOneStrategy, Props}
+import akka.event.Logging
 
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+class MyActor extends Actor {
+  val log = Logging(context.system, this)
+
+  val child = context.actorOf(Props[MyActor2], name = "myChild")
+
+  override val supervisorStrategy = OneForOneStrategy(maxNrOfRetries = 10, withinTimeRange = 1 minute) {
+    case _: ArithmeticException => Resume
+    case _: NullPointerException => Restart
+    case _: IllegalArgumentException => Stop
+    case _: Exception => Escalate
+  }
+
+  def receive = {
+    case s: String => {
+      log.info(s)
+      child ! s
+    }
+    case _ => {
+    }
+  }
 }
